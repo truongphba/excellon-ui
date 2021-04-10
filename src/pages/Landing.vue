b9285da497cc70c3dac89a43c4a0b1a79554fa92
Macbook <nguyendangbanghn93@gmail.com>
2021-04-10 23:33:19 +0700

    landing page

--- src/pages/Landing.vue
+++ src/pages/Landing.vue
@@ -1,7 +1,148 @@
 <template>
-<div>
-  <h4>Landing Page</h4>
-</div>
+  <div>
+    <div class="container">
+      <div class=" df jcsb">
+        <img class="" src="https://www.hortex-vietnam.com/wp-content/uploads/2019/04/LOGO_HORTEX_VN_2020-300-px-200x111.jpg" alt="">
+        <div class="df">
+          <div class="cgd tduh shineh h1 df aic">
+            <a class="bấmĐc" tel = '0989859398'>Hotline: 19001088</a>
+          </div>
+          <div class="cgd tduh shineh h1 df aic ml15 bấmĐc">Đăng nhập</div>
+        </div>
+      </div>
+    </div>
+    <div class="bgcgd">
+      <div class="container df jcsb">
+        <div class="pa10 plr15 cf shineh bấmĐc fs12 ">Giới thiệu</div>
+        <div class="pa10 plr15 cf shineh bấmĐc fs12 ">Sản phẩm</div>
+        <div class="pa10 plr15 cf shineh bấmĐc fs12 ">Dịch vụ</div>
+        <div class="pa10 plr15 cf shineh bấmĐc fs12 ">Liên hệ</div>
+      </div>
+    </div>
+    <div class="bgrn bgpc bgsc w1" style="background-image: url('https://cdn.inevn.com/img/thumb/83712.1kx.isij'); height:500px">
+    </div>
+    <div class="container">
+      <div class="ptb100">
+        <div class="cb">
+          <div class="col-xs-6 plr15">
+            <div class="fs2">Call Center toàn diện cho mọi doanh nghiệp</div>
+            <div class="c3 fs15 mt15">Với sự linh hoạt và dễ dàng mở rộng, chúng tôi cung cấp giải pháp Call Center chuyên nghiệp và tối ưu chi phí cho mọi doanh nghiệp (SMEs, Startups, Enterprises).</div>
+            <div class="grid">
+              <div class="btn bn bgcgd cf pa10 plr25 bra5 mt25">Liên hệ tư vấn >></div>
+            </div>
+          </div>
+          <div class="col-xs-6">
+            <div class="bgrn bgpc bgsc w1" style="background-image: url('https://i1.wp.com/cdn.inevn.com/img/thumb/93496.500x.isij?resize=475,237&w=475&quality=100&strip=all'); height:350px"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="ptb100 fs25 tac bg1o1">
+      Dễ dàng triển khai, quản lý và mở rộng <br>
+      hệ thống tổng đài không giới hạn
+    </div>
+    <div class="container">
+      <div class="pt100 pb25">
+        <div class="df aic">
+          <div class="col-xs-6">
+            <div class="bgrn bgpc bgsc w1" style="background-image: url('https://cdn.inevn.com/img/thumb/83715.1kx.isij'); height:350px"></div>
+          </div>
+          <div class="col-xs-6 plr15">
+            <div class="fs2">Triển khai đơn giản trong vài phút</div>
+            <div class="c3 fs15 mt15">Không chi phí phần cứng, không nhân sự IT vận hành, bạn chỉ cần đăng nhập là có thể sử dụng và trải nghiệm ngay giải pháp Call Center chuyên nghiệp.</div>
+            <div class="grid">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="pb100 pt25">
+        <div class="df aic">
+          <div class="col-xs-6 plr15">
+            <div class="fs2">Tích hợp dễ dàng, nhanh chóng</div>
+            <div class="c3 fs15 mt15">Tự động đồng bộ các thông tin từ phần mềm CRM, ERP, Helpdesk của doanh nghiệp với Call Center.
+              <br><br>
+              Bạn có thể nghe lại file ghi âm của tất cả các cuộc gọi, đồng thời kiểm tra lịch sử cuộc gọi, tài khoản, leads ngay trên chính phần mềm CRM của mình.
+            </div>
+          </div>
+          <div class="col-xs-6">
+            <div class="bgrn bgpc bgsc w1" style="background-image: url('https://i1.wp.com/cdn.inevn.com/img/thumb/93496.500x.isij?resize=475,237&w=475&quality=100&strip=all'); height:350px"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="ptb100 bg1o1">
+      <div class="container">
+        <div class="fs25 fwb tac pb50">Đơn giản cho cả quản trị viên và nhân viên tổng đài</div>
+        <div class="df">
+          <div class="col-xs-4 pa10">
+            <div class="b1sd bóngh pa10 bra5 bgcf">
+              <div class="pt35 pb15">
+                <div class="wh50 bgrn bgpc bgsc maa" style="background-image: url('https://v2.stringee.com/stringeex-landingpage-wp/wp-content/uploads/2020/07/1-click-e1594020570638.png');"></div>
+                <div class="fs14 fwb tac ptb25">1 click</div>
+                <div class="tac plr50 c3 fs14 pb50">Tích hợp nhanh chóng với tất cả các phần mềm CRM, Helpdesk của doanh nghiệp. Nhân viên chỉ cần một click ngay trên CRM là có thể gọi ngay tới khách hàng.</div>
+              </div>
+            </div>
+          </div>
+          <div class="col-xs-4 pa10">
+            <div class="b1sd bóngh pa10 bra5 bgcf">
+              <div class="pt35 pb15">
+                <div class="wh50 bgrn bgpc bgsc maa" style="background-image: url('https://v2.stringee.com/stringeex-landingpage-wp/wp-content/uploads/2020/07/Call-center-1.png');"></div>
+                <div class="fs14 fwb tac ptb25">Điều khiển trực quan</div>
+                <div class="tac plr50 c3 fs14 pb50">Rút ngắn thời gian đào tạo từ vài tháng xuống còn vài phút. Nhân viên của bạn có thể dễ dàng sử dụng tổng đài với các thao tác đơn giản nhất.</div>
+              </div>
+            </div>
+          </div>
+          <div class="col-xs-4 pa10">
+            <div class="b1sd bóngh pa10 bra5 bgcf">
+              <div class="pt35 pb15">
+                <div class="wh50 bgrn bgpc bgsc maa" style="background-image: url('https://v2.stringee.com/stringeex-landingpage-wp/wp-content/uploads/2020/07/Call-center-3.png');"></div>
+                <div class="fs14 fwb tac ptb25">Quản lý dễ dàng</div>
+                <div class="tac plr50 c3 fs14 pb50">Không cần biết kỹ thuật, nhà quản lý có thể thêm số mới, phân chia công việc và giám sát nhân viên mọi lúc, mọi nơi (trên desktop và smartphone).</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- <div class=""> -->
+        <!-- <div class="df aic">
+          <div class="col-xs-6 plr15">
+            <div class="fs2">Tích hợp dễ dàng, nhanh chóng</div>
+            <div class="c3 fs15 mt15">Tự động đồng bộ các thông tin từ phần mềm CRM, ERP, Helpdesk của doanh nghiệp với Call Center.
+              <br><br>
+              Bạn có thể nghe lại file ghi âm của tất cả các cuộc gọi, đồng thời kiểm tra lịch sử cuộc gọi, tài khoản, leads ngay trên chính phần mềm CRM của mình.
+            </div>
+          </div>
+          <div class="col-xs-6">
+            <div class="bgrn bgpc bgsc w1" style="background-image: url('https://i1.wp.com/cdn.inevn.com/img/thumb/93496.500x.isij?resize=475,237&w=475&quality=100&strip=all'); height:350px"></div>
+          </div>
+        </div> -->
+        <!-- </div> -->
+      </div>
+    </div>
+    <div class="bgcgd">
+      <div class="container">
+        <div class="df">
+          <div class="col-xs-4 pa25 cf lh2">
+            <div class="fwb fs12">Liên hệ</div>
+            <div>CÔNG TY CỔ PHẦN STRINGEE</div>
+            <div>Trụ sở: Tầng 16, VP-2, toà Sun Square, 21 Lê Đức Thọ, Nam Từ Liêm, Hà Nội</div>
+            <div>Tổng đài hỗ trợ: <b>19001088</b></div>
+            <div>Email: <b>info@stringee.com</b></div>
+          </div>
+          <div class="col-xs-4 pa25 cf lh2">
+            <div class="fwb fs12">Dịch vụ</div>
+            <div class="tduh bấmĐc">In bound</div>
+            <div class="tduh bấmĐc">Out bound</div>
+            <div class="tduh bấmĐc">Service</div>
+          </div>
+          <div class="col-xs-4 pa15">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d958.5193680006109!2d105.94682156396603!3d20.995483579225944!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1svi!2s!4v1617987717350!5m2!1svi!2s" width="100%" height="250" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,7 +150,18 @@ export default {
   name: 'Landing'
 }
 </script>
-
 <style scoped>
-
+.ptb100{
+  padding-bottom: 100px;
+  padding-top: 100px;
+}
+.pt100{
+  padding-top: 100px;
+}
+.pb100{
+  padding-bottom: 100px;
+}
+.bg1o1{
+  background: #acc2a726;
+}
 </style>
